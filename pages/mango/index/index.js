253d3b5544129f47b56bff91afd5c975d568a268
Unknown <xuce_zzu@163.com>
2018-04-17 10:36:33 +0800

    提交用户页面的一些逻辑

--- pages/mango/index/index.js
+++ pages/mango/index/index.js
@@ -1,66 +1,96 @@
+// pages/mango/index/index.js
+// 首页逻辑
 
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    name: "I Love You ..."
+    color: 'red',
+    text: 'Name',
+    message: 'I Love You ...',
+    id: 0,
+    longitude:'',
+    latitude:'',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
-  }
+
+  },
+
+  /**
+   * 改变颜色
+   */
+  changeColor: function (event) {
+    console.log(event)
+    var name = event.target.dataset.name;
+    this.setData({
+      color: getRandomColor(),
+      text: name + " Hello !"
+    })
+  },
 })
+
+// 获取随机颜色
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color;
+}
